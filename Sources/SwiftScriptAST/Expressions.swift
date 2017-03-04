5df3619f451632b5a5116eb658e889a6ef46de9a
Yuta Koshizawa <koher@koherent.org>
2017-03-04 11:42:00 +0900

    implement expressions

--- Sources/SwiftScriptAST/Expressions.swift
+++ Sources/SwiftScriptAST/Expressions.swift
@@ -1,3 +1,68 @@
 public protocol Expression: Node {
     
 }
+
+public struct IdentifierExpression: Expression {
+    var identifier: String
+}
+
+public struct SelfExpression: Expression {
+    
+}
+
+public struct SuperclassExpression: Expression {
+    
+}
+
+public struct ClosureExpression: Expression {
+    // TODO
+}
+
+public struct ParenthesizedExpression: Expression {
+    var expression: Expression
+}
+
+public struct TupleExpression: Expression {
+    var elements: [(String?, Expression)]
+}
+
+public struct ImplicitMemberExpression: Expression {
+    // Unsupported
+}
+
+public struct WildcardExpression: Expression {
+    
+}
+
+public struct FunctionCallExpression: Expression {
+    var arguments: [(String?, Expression)]
+    var trailingClosure: ClosureExpression?
+}
+
+public struct InitializerExpression: Expression {
+    
+}
+
+public struct ExplicitMemberExpression: Expression {
+    var expression: Expression
+    var member: String
+}
+
+public struct PostfixSelfExpression: Expression {
+    // Unsupported
+}
+
+public struct DynamicTypeExpression: Expression {
+    var expression: Expression
+}
+
+public struct SubscriptExpression: Expression {
+    var expression: Expression
+    var arguments: [Expression]
+}
+
+public struct OptionalChainingExpression: Expression {
+    var expression: Expression
+    var member: String
+}
+
