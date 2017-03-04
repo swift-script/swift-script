76ed216891425a84d5e3d2c810697b0bbfeb85c7
Yuta Koshizawa <koher@koherent.org>
2017-03-04 17:40:44 +0900

    implement `transpile`

--- Sources/Transpile.swift
+++ Sources/Transpile.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 public func transpile(code: String) throws -> String {
-    return "Hello, \(Date())!"
+    let statements: [Statement] = try parseTopLevel(code)
+    let jsStatements: [String] = statements.map { $0.javaScript(with: 0) }
+    return jsStatements.joined(separator: "\n")
 }
