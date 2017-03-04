8354415c7958dfb07f77b167e7786a87da408783
masaru-ichikawa <masaru-ichikawa@cookpad.com>
2017-03-04 17:40:06 +0900

    implemente expression

--- Sources/TranslateExpressions.swift
+++ Sources/TranslateExpressions.swift
@@ -15,3 +15,72 @@ extension FunctionCallExpression {
         return "\(f)(\(argsString))"
     }
 }
+
+extension SelfExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        return "this"
+    }
+}
+
+extension SuperclassExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        return "super"
+    }
+}
+
+extension ClosureExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        let args = (arguments.map { $0.0 }).joined(separator: ", ")
+        let lines = statements.map { $0.javaScript(with: indentLevel + 1) }.joined(separator: "\n")
+        switch statements.count {
+        case 0:
+            return "(\(args)) => {}"
+        case 1:
+            return "(\(args)) => \(lines)"
+        default:
+            let spaces = String(repeating: "    ", count: indentLevel)
+            return "(\(args)) => {\n\(lines)\n\(spaces)}"
+        }
+    }
+}
+
+extension ParenthesizedExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        return "(\(expression.javaScript(with: indentLevel + 1)))"
+    }
+}
+
+extension TupleExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        let values = elements.map { $0.1.javaScript(with: indentLevel + 1) }.joined(separator: ", ")
+        return "[\(values)]"
+    }
+}
+
+extension WildcardExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        return "_"
+    }
+}
+
+extension ExplicitMemberExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        let variable = expression.javaScript(with: indentLevel + 1)
+        return "\(variable).\(member)"
+    }
+}
+
+extension SubscriptExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        let variable = expression.javaScript(with: indentLevel + 1)
+        let args = arguments.map { $0.javaScript(with: indentLevel + 1) }.joined(separator: ", ")
+        return "\(variable)[\(args)]"
+    }
+}
+
+extension OptionalChainingExpression {
+    public func javaScript(with indentLevel: Int) -> String {
+        let exp = expression.javaScript(with: indentLevel + 1)
+        return "q(\(exp), (x) => x.\(member)"
+    }
+}
