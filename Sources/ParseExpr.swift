dd2ea346f59c884d7e570a29316909cd8e89bf05
Rintaro Ishizaki <fs.output@gmail.com>
2017-03-04 14:56:28 +0900

    [Parse] parse basic expresssions

--- Sources/ParseExpr.swift
+++ Sources/ParseExpr.swift
@@ -2,20 +2,67 @@ import Runes
 import TryParsec
 
 
-///
+fileprivate func asExpr(expr: Expression) -> Expression {
+    return expr
+}
+
+
+func _exprSequencceElement() -> SwiftParser<Expression> {
+    let parseTry
+        = ({ _ in "try!" } <^> kw_try *> char("?") *> WS)
+            <|> ({ _ in "try?" } <^> kw_try *> char("?") *> WS)
+            <|> ({ _ in "try" } <^> kw_try *> WS)
+            <|> pure(nil)
+    return { op in { subExpr in
+        op != nil ? PrefixUnaryOperation(operatorSymbol: op!, operand: subExpr) : subExpr }}
+        <^> parseTry
+        <*> exprUnary
+}
+
+
+let exprUnary = _exprUnary()
+func _exprUnary() -> SwiftParser<Expression> {
+    return { op in { subExpr in
+        op != nil ? PrefixUnaryOperation(operatorSymbol: op!, operand: subExpr) : subExpr }}
+        <^> zeroOrOne(oper_prefix)
+        <*> exprAtom
+}
+
+let exprAtom = _exprAtom()
+func _exprAtom() -> SwiftParser<Expression> {
+    return exprPrimitive >>- exprSuffix
+}
+
+//-----------------------------------------------------------------------
+// primitive expressions
+
+let exprPrimitive = _exprPrimitive()
+func _exprPrimitive() -> SwiftParser<Expression> {
+    return (exprStringLiteral <&> asExpr)
+        <|> (exprFloatLiteral <&> asExpr)
+        <|> (exprIntegerLiteral <&> asExpr)
+        <|> (exprArrayLiteral <&> asExpr)
+        <|> (exprDictionaryLiteral <&> asExpr)
+        <|> (exprIdentifier <&> asExpr)
+        <|> (exprParenthesized <&> asExpr)
+        <|> (exprTuple <&> asExpr)
+        <|> (exprTuple <&> asExpr)
+}
+
+
 let exprIdentifier = _exprIdentifier()
 func _exprIdentifier() -> SwiftParser<IdentifierExpression> {
-    return fail("not implemented")
+    return IdentifierExpression.init <^> identifier
 }
 
 let exprSelf = _exprSelf()
 func _exprSelf() -> SwiftParser<SelfExpression>  {
-    return fail("not implemented")
+    return { _ in SelfExpression() } <^> kw_self
 }
 
 let exprSuper = _exprSuper()
 func _exprSuper() -> SwiftParser<SuperclassExpression>  {
-    return fail("not implemented")
+    return { _ in SuperclassExpression() } <^> kw_Self
 }
 
 let exprClosure = _exprClosure()
@@ -38,44 +85,54 @@ func _exprImplicitMember() -> SwiftParser<ImplicitMemberExpression>  {
     return fail("not implemented")
 }
 
+
 let exprWildcard = _exprWildcard()
 func _exprWildcard() -> SwiftParser<WildcardExpression> {
     return fail("not implemented")
 }
 
-let exprPostfixSelf = _exprPostfixSelf()
-func _exprPostfixSelf() -> SwiftParser<PostfixSelfExpression>  {
+//-----------------------------------------------------------------------
+// suffix expressions
+
+func exprSuffix(subj: Expression) -> SwiftParser<Expression> {
+    return (_exprPostfixSelf(subj) >>- exprSuffix)
+        <|> (_exprInitializer(subj) >>- exprSuffix)
+        <|> (_exprExplicitMember(subj) >>- exprSuffix)
+        <|> (_exprFunctionCall(subj) >>- exprSuffix)
+        <|> (_exprSubscript(subj) >>- exprSuffix)
+        <|> (_exprOptionalChaining(subj) >>- exprSuffix)
+        <|> (_exprInitializer(subj) >>- exprSuffix)
+        <|> (_exprInitializer(subj) >>- exprSuffix)
+//        <|> (_exprTrailingClosure(subj) >>- exprSuffix)
+}
+
+func _exprPostfixSelf(_ subj: Expression) -> SwiftParser<PostfixSelfExpression>  {
     return fail("not implemented")
 }
 
-let exprDynamicType = _exprDynamicType()
-func _exprDynamicType() -> SwiftParser<DynamicTypeExpression> {
+func _exprFunctionCall(_ subj: Expression) -> SwiftParser<FunctionCallExpression> {
     return fail("not implemented")
 }
 
-let exprFunctionCall = _exprFunctionCall()
-func _exprFunctionCall() -> SwiftParser<FunctionCallExpression> {
-    return fail("not implemented")
-}
-
-let exprInitializer =  _exprInitializer()
-func _exprInitializer() -> SwiftParser<InitializerExpression> {
+func _exprInitializer(_ subj: Expression) -> SwiftParser<InitializerExpression> {
     return fail("not iomplemented")
 }
 
 
-let exprExplicitMemeber = _exprExplicitMember()
-func _exprExplicitMember() -> SwiftParser<ExplicitMemberExpression> {
+func _exprExplicitMember(_ subj: Expression) -> SwiftParser<ExplicitMemberExpression> {
     return fail("not implemented")
 }
 
 
-let exprSubscript = _exprSubscript()
-func _exprSubscript() -> SwiftParser<SubscriptExpression> {
+func _exprSubscript(_ subj: Expression) -> SwiftParser<SubscriptExpression> {
     return fail("not implemented")
 }
 
-let exprOptionalChanining = _exprOptionalChaining()
-func _exprOptionalChaining() -> SwiftParser<OptionalChainingExpression> {
+func _exprOptionalChaining(_ subj: Expression) -> SwiftParser<OptionalChainingExpression> {
     return fail("not implemented")
 }
+
+func _exprDynamicType(_ subj: Expression) -> SwiftParser<DynamicTypeExpression> {
+    return fail("not implemented")
+}
+
--- Sources/ParseLiteral.swift
+++ Sources/ParseLiteral.swift
@@ -6,28 +6,29 @@ import TryParsec
 
 let exprNilLiteral = _exprNilLiteral()
 func _exprNilLiteral() -> SwiftParser<NilLiteral> {
-    return fail("not implemented")
+    return { _ in NilLiteral() } <^> kw_nil
 }
 
 let exprBooleanLiteral = _exprBooleanLiteral()
 func _exprBooleanLiteral() -> SwiftParser<BooleanLiteral> {
-    return fail("not implemented")
+    return ({ _ in BooleanLiteral(value: false) } <^> kw_false)
+        <|> ({ _ in BooleanLiteral(value: true) } <^> kw_true)
 }
 
 
 let exprStringLiteral = _exprStringLiteral()
 func _exprStringLiteral() -> SwiftParser<StringLiteral> {
-    return fail("not implemented")
+    return { value in StringLiteral(value: value) } <^> stringLiteral
 }
 
 let exprIntegerLiteral = _exprIntegerLiteral()
 func _exprIntegerLiteral() -> SwiftParser<IntegerLiteral> {
-    return fail("not implemented")
+    return { value in IntegerLiteral(value: value) } <^> integerLiteral
 }
 
 let exprFloatLiteral = _exprFloatLiteral()
 func _exprFloatLiteral() -> SwiftParser<FloatingPointLiteral> {
-    return fail("not implemented")
+    return { value in FloatingPointLiteral(value: value) } <^> floatLiteral
 }
 
 let exprArrayLiteral = _exprArrayLiteral()
