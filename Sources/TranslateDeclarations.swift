e09710b979588a4ebe8dbccd624102fda8bc13b7
Yuta Koshizawa <koher@koherent.org>
2017-03-05 09:32:33 +0900

    implement `VariableDeclaration`

--- Sources/TranslateDeclarations.swift
+++ Sources/TranslateDeclarations.swift
@@ -12,3 +12,18 @@ extension ConstantDeclaration {
         }
     }
 }
+
+extension VariableDeclaration {
+    public func javaScript(with indentLevel: Int) -> String {
+        guard !isStatic else {
+            // static properties must be translated in type layers
+            return ""
+        }
+        
+        if let expression = expression {
+            return "\(indent(of: indentLevel))let \(name) = \(expression.javaScript(with: indentLevel));"
+        } else {
+            return "\(indent(of: indentLevel))let \(name);"
+        }
+    }
+}
