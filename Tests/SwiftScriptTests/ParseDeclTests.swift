a3575c52ffad7ac648de8be18372b34183dbde4d
Rintaro Ishizaki <fs.output@gmail.com>
2017-03-29 15:14:07 +0900

    [Test] Update some tests using Equtable

--- Tests/SwiftScriptTests/ParseDeclTests.swift
+++ Tests/SwiftScriptTests/ParseDeclTests.swift
@@ -3,20 +3,99 @@ import XCTest
 
 class ParseDeclTests: XCTestCase {
     func testDeclFunction() {
-        XCTAssertTrue(parseSuccess(
+        ParseEqual(
             declFunction,
             "func foo(x: A = 1, x y: B) throws -> C {\n"
                 + "  return foo\n"
-                + "}"))
-        XCTAssertTrue(parseSuccess(
+                + "}",
+            FunctionDeclaration(isStatic: false,
+                name: "foo",
+                arguments: [
+                    Parameter(
+                        externalParameterName: nil,
+                        localParameterName: "x",
+                        type: TypeIdentifier­.init(names: ["A"]),
+                        defaultArgument: IntegerLiteral(value: 1)
+                    ),
+                    Parameter(
+                        externalParameterName: "x",
+                        localParameterName: "y",
+                        type: TypeIdentifier­.init(names: ["B"]),
+                        defaultArgument: nil
+                    ),
+                ],
+                result: TypeIdentifier­(names: ["C"]),
+                hasThrows: true,
+                body: [
+                    ReturnStatement(
+                        expression: IdentifierExpression(identifier: "foo")
+                    ),
+                ]
+            )
+        )
+        
+        ParseEqual(
             declFunction,
-            "func foo(x:A=1,x y:B)throws->C{return foo}"))
-        XCTAssertTrue(parseSuccess(
+            "func foo(x:A=1,x y:B)throws->C{return foo}",
+            FunctionDeclaration(
+                isStatic: false,
+                name: "foo",
+                arguments: [
+                    Parameter(
+                        externalParameterName: nil,
+                        localParameterName: "x",
+                        type: TypeIdentifier­.init(names: ["A"]),
+                        defaultArgument: IntegerLiteral(value: 1)
+                    ),
+                    Parameter(
+                        externalParameterName: "x",
+                        localParameterName: "y",
+                        type: TypeIdentifier­.init(names: ["B"]),
+                        defaultArgument: nil
+                    ),
+                ],
+                result: TypeIdentifier­(names: ["C"]),
+                hasThrows: true,
+                body: [
+                    ReturnStatement(
+                        expression: IdentifierExpression(identifier: "foo")
+                    ),
+                ]
+            )
+        )
+
+        ParseEqual(
             declFunction,
-            "static func foo() { }"))
-        XCTAssertTrue(parseSuccess(
+            "static func foo() { }",
+            FunctionDeclaration(
+                isStatic: true,
+                name: "foo",
+                arguments: [],
+                result: nil,
+                hasThrows: false,
+                body: []
+            )
+        )
+        
+        ParseEqual(
             declFunction,
-            "func foo<T>(x:T) throws -> C where T == X {}"))
+            "func foo<T>(x:T) throws -> C where T == X {}",
+            FunctionDeclaration(
+                isStatic: false,
+                name: "foo",
+                arguments: [
+                    Parameter(
+                        externalParameterName: nil,
+                        localParameterName: "x",
+                        type: TypeIdentifier­.init(names: ["T"]),
+                        defaultArgument: nil
+                    ),
+                ],
+                result: TypeIdentifier­(names: ["C"]),
+                hasThrows: true,
+                body: []
+            )
+        )
     }
     
     func testDeclInitializer() {
