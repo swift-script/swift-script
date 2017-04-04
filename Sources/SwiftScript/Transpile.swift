6d44bc79eb23c78fffb58d604344311c6f461d46
Yuta Koshizawa <koher@koherent.org>
2017-04-04 13:22:45 +0900

    [Parse] Rename `parseTopLevel` to `parse`

--- Sources/SwiftScript/Transpile.swift
+++ Sources/SwiftScript/Transpile.swift
@@ -4,6 +4,6 @@ import SwiftScriptParse
 import SwiftScriptTranslate
 
 public func transpile(code: String) throws -> String {
-    let statements: [Statement] = try parseTopLevel(code)
+    let statements: [Statement] = try parse(code)
     return try translateStatements(statements: statements, indentLevel: 0)
 }
