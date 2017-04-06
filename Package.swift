5b3869326c5d3db8eaaa737008326622e281ad4b
Yuta Koshizawa <koher@koherent.org>
2017-04-06 19:59:52 +0900

    Add `SwiftAST` to `SwiftScript`'s dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     targets: [
         Target(name: "SwiftAST"),
         Target(name: "SwiftParse", dependencies: ["SwiftAST"]),
-        Target(name: "SwiftScript", dependencies: ["SwiftParse"]),
+        Target(name: "SwiftScript", dependencies: ["SwiftAST", "SwiftParse"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/koher/TryParsec.git", "0.1.1"),
