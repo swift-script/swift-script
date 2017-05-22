e4daa9db56c6fb20905fed142c6704bc422021b3
Yasuhiro Inami <inamiy@gmail.com>
2017-05-23 02:36:54 +0900

    Remove logging

--- Sources/SwiftKotlin/Transpile.swift
+++ Sources/SwiftKotlin/Transpile.swift
@@ -4,6 +4,5 @@ import SwiftParse
 
 public func transpile(_ code: String) throws -> String {
     let statements: [Statement] = try parse(code)
-    print(statements)
     return try translate(statements, with: 0)
 }
